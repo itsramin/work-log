4030c9e021fd0a84fcfe34150c2acf5a4d604158
Ramin <Raminshakooei@gmail.com>
2023-04-22 00:59:57 +0330

    test pwa

--- src/index.js
+++ src/index.js
@@ -17,3 +17,9 @@ root.render(
     </PersistGate>
   </Provider>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
---
+++ src/sw.js
@@ -0,0 +1,19 @@
+// service-worker.js
+const cacheName = "your-app-name";
+const assets = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
